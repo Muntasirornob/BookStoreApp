5e2596ba4365a0732500a0fe2c2952380130e590
Muntasir Chowdhury <muntasirornob@gmail.com>
2024-11-08 23:17:36 -0700

    adding HomePage Layout

--- src/pages/home/components/Home.js
+++ src/pages/home/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Hero} from "./Hero"
+import {FeaturedProducts} from "./FeatureProduct"
+import {Testimonial} from "./Testomonial"
+import {Faq}  from "./Faq"
 
 export const Home = () => {
   return (
     <div>
         
+        <Hero/>
+        <FeaturedProducts />
+        <Testimonial/>
+        <Faq />
+      
     </div>
   )
 }
