189435a08c5f1c759e9a64c27b9fb27f48f37daf
Muntasir Chowdhury <muntasirornob@gmail.com>
2024-11-15 19:40:49 -0700

    Home page layout fix

--- src/pages/home/components/Faq.js
+++ src/pages/home/components/Faq.js
@@ -25,7 +25,7 @@ export const Faq = () => {
     ];
     
   return (
-    <section className="my-10 p-7 border rounded dark:border-slate-700 shadow-sm">        
+    <section className="my-10 p-7 border rounded dark:border-slate-700 shadow-sm mx-auto max-w-screen-xl px-4 md:px-6 py-3">        
       <h1 className="text-2xl text-center font-semibold dark:text-slate-100 mb-3 underline underline-offset-8">Question in mind?</h1>    
             <div className="" id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
               { faqs.map((faq) => (
